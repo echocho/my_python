e69527e297535239bd51427235d2f502c8b07aea
Zihui Zheng <zihui@example.com>
2018-04-10 13:30:17 +0800

    Chpt 7. An example about divide don't understand.

---
+++ divide_ex_p253.sql
@@ -0,0 +1,9 @@
+select distinct emp
+from EmpSkills ES1
+where not exists
+			(select skill 
+			  from Skills
+			 except 
+			   select skill
+			    from EmpSkills ES2 
+			 where ES1.emp = ES2.emp);
