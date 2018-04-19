a0da4df6375ddca53329542125540481604c0081
Zihui Zheng <zihui@example.com>
2018-04-19 18:03:16 +0800

    pygame. Snake v5. Manu/intro frame added. Start and Quit bottons added, with mouse hover effect. Game over message function added.

--- snake.py
+++ snake.py
@@ -1,4 +1,4 @@
-import pygame, random, copy
+import pygame, random, time
 pygame.init()
 
 # -------------------
@@ -9,20 +9,19 @@ SCREEN_HEIGHT = 500
 SCREEN_SIZE = (SCREEN_WIDTH, SCREEN_HEIGHT)
 BORDER_THICKNESS = 20
 FOOD_AREA_WIDE = BORDER_THICKNESS
-FOOD_AREA_HEIGHT = SCREEN_HEIGHT - 3*BORDER_THICKNESS
+FOOD_AREA_HEIGHT = SCREEN_HEIGHT - 1.5*BORDER_THICKNESS
 SNAKE_RECT_LEN = 5
 INITIAL_SNAKE_LEN = 200
 BLACK = (0, 0, 0)
 GREEN = (20, 255, 140)
-GREY = (210, 210, 210)
+LIGHT_GREY = (210, 210, 210)
+GREY = (190, 190, 190)
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 PURPLE = (255, 0, 255)
 YELLOW = (255, 255, 0)
 CYAN = (0, 255, 255)
 BLUE = (100, 100, 255)
-
-# FIXME: will fix dotted snake when change of speed
 SPEED = 5
 # -------------------
 # -------------------
@@ -53,8 +52,89 @@ foodList.append((food_x_pos, food_y_pos))
 
 direction_vector = (1, 0)
 
+def gameOver():
+    message_display('Game Over!')
+ 
+def message_display(text):
+    set_textFont = pygame.font.Font('freesansbold.ttf',65)
+    textSurface, textArea = text_objects(text, set_textFont)
+    textArea.center = ((SCREEN_WIDTH/2),(SCREEN_HEIGHT/2))
+    screen.blit(textSurface, textArea)
+ 
+    pygame.display.update()
+ 
+    time.sleep(3)
+
+def text_objects(text, font):
+    textSurface = font.render(text, True, GREY)
+    return textSurface, textSurface.get_rect()
+
+def botton_text_objects(text, font, color= None):
+    textSurface = font.render(text, True, WHITE)
+    return textSurface, textSurface.get_rect() 
+
+def start_botton():
+    pygame.draw.rect(screen, GREY, [100, 350, 100, 50])
+    # add mouse interaction
+    mouse = pygame.mouse.get_pos()
+    if 100+100 > mouse[0] > 100 and 350+50 > mouse[1] > 350:
+        pygame.draw.rect(screen, LIGHT_GREY, [100, 350, 100, 50])
+    set_textFont = pygame.font.Font('freesansbold.ttf', 25)
+    textSurface, textArea = botton_text_objects('START', set_textFont)
+    textArea.center = (150,380)
+    screen.blit(textSurface, textArea)
+
+def quit_botton():
+    pygame.draw.rect(screen, GREY, [300, 350, 100, 50])
+    # add mouse interaction
+    mouse = pygame.mouse.get_pos()
+    if 300+100 > mouse[0] > 300 and 350+50 > mouse[1] > 350:
+        pygame.draw.rect(screen, LIGHT_GREY, [300, 350, 100, 50])
+    set_font = pygame.font.Font('freesansbold.ttf', 25)
+    textSurface, textArea = botton_text_objects('QUIT', set_font)
+    textArea.center = (350,380)
+    screen.blit(textSurface, textArea)
+
+
+        
+
+
+
+
+
+
+def game_intro():
+    intro = True
+    while intro:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+        screen.fill(BLACK)
+        set_textFont = pygame.font.Font('freesansbold.ttf', 40)
+        textSurface, textArea = text_objects('Snake of Simplicity', set_textFont)
+        textArea.center = ((SCREEN_WIDTH/2), (SCREEN_HEIGHT/4))
+        screen.blit(textSurface, textArea)
+        # draw start and quit buttons
+        start_botton()
+        quit_botton()
+            
+        pygame.display.update()
+        clock.tick(15)
+        #-----------------
+        # Question and experiment:
+        # Or we could change message_display() abit and use "message_display('Snake of Simplicity')" instead?
+        #-----------------   
+game_intro()
+
+        
+
+
+
+
+
 # main program loop
 while carryOn:
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             carryOn = False
@@ -100,13 +180,15 @@ while carryOn:
 
     # check if snake hits border
     for (snake_rect_x_pos, snake_rect_y_pos) in bodyList:
-        if snake_rect_x_pos < FOOD_AREA_WIDE or snake_rect_x_pos >= FOOD_AREA_HEIGHT or snake_rect_y_pos < FOOD_AREA_WIDE or snake_rect_y_pos >= FOOD_AREA_HEIGHT:
+        if snake_rect_x_pos < BORDER_THICKNESS or snake_rect_x_pos > SCREEN_WIDTH-BORDER_THICKNESS or snake_rect_y_pos < BORDER_THICKNESS or snake_rect_y_pos > SCREEN_HEIGHT-BORDER_THICKNESS:
             print('hit border-game over')
+            gameOver()
             carryOn = False
 
     # check if snake collaps with itself:
     if bodyList[-1] in bodyList[:-1]:
         print('hit itself-game over')
+        gameOver
         carryOn = False
     
     pygame.display.flip()
